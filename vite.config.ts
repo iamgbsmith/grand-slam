9439210539bfc30885fdd6368ced863d060e8501
Greg Smith <25718868+iamgbsmith@users.noreply.github.com>
2025-07-06 19:08:15 +1200

    Updated Vite configuration to load Tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/grand-slam/',
   plugins: [
+    tailwindcss(),
     react(),
     VitePWA({
       registerType: 'autoUpdate',
